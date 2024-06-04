76314db42782690af4a6f5ac2e2e33f827052c6e
Nakshatrajoshi <91380430+Nakshatrajoshi@users.noreply.github.com>
2024-06-04 18:54:32 +0530

    Add files via upload

---
+++ project/ChatClient.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.net.*;
+
+public class ChatClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 12346;
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in))) {
+
+            new Thread(new IncomingMessageHandler(in)).start();
+
+            String userInput;
+            while ((userInput = consoleInput.readLine()) != null) {
+                out.println(userInput);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class IncomingMessageHandler implements Runnable {
+        private BufferedReader in;
+
+        public IncomingMessageHandler(BufferedReader in) {
+            this.in = in;
+        }
+
+        public void run() {
+            try {
+                String message;
+                while ((message = in.readLine()) != null) {
+                    // System.out.println(message);
+                    // Send a response back to the client
+                    
+                String response = "Server: " + message;
+                // String response;
+                switch (message) {
+                    case "hello":
+                        response = "Server: Hello back to you!";
+                        break;
+                    case "goodbye":
+                        response = "Server: Goodbye, see you later!";
+                        break;
+                    default:
+                        response = "Server: I didn't understand that.";
+                        break;
+                }
+                System.out.println(response);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
